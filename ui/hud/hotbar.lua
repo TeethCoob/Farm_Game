d1f44e28a55bfefe9baeb3dd2517b6f40e8c5e92
TeethCoob <istafa879@gmail.com>
2025-08-10 19:04:11 +0700

    hotbar

--- ui/hud/hotbar.lua
+++ ui/hud/hotbar.lua
@@ -10,18 +10,22 @@ local hotbar = inventory.hotbar
 
 -- PROPERTIES
 hotbarUI.sprite = loadTexture('assets/textures/ui/hud/hotbar.png')
-hotbarUI.selection_sprite = loadTexture('assets/textures/ui/hud/hotbar_selection.png')
+hotbarUI.selection_outline_sprite = loadTexture('assets/textures/ui/hud/hotbar_selection_outline.png')
 hotbarUI.position = {}
 hotbarUI.scale = 0.7
 hotbarUI.scale_offset = {
     X = hotbarUI.sprite:getWidth()/2,
     Y = hotbarUI.sprite:getHeight()/2
 }
+hotbarUI.selection_outline_scale = 0.7
+hotbarUI.selection_outline_scale_offset = {
+    X = hotbarUI.selection_outline_sprite:getWidth()/2,
+    Y = hotbarUI.selection_outline_sprite:getHeight()/2
+}
 hotbarUI.grid = {
     padding                = 54,
     item_scale             = 2,
     item_scale_offset      = {},
-    origin_position        = {},
     origin_position_offset = {
         X = 1,
         Y = 3
@@ -34,34 +38,53 @@ function hotbarUI:draw()
 
     self.position.X = screenW / 2
     self.position.Y = screenH * 0.95
+    
+    self.grid.origin_position = {
+        X = hotbarUI.position.X - hotbarUI.sprite:getWidth()/3.25,
+        Y = hotbarUI.position.Y
+    }
+    
+    -- SETS OFFSET TO GRID'S ORIGIN POSITION FOR ELEMENTS
+    hotbarUI.grid.origin_position.X = hotbarUI.grid.origin_position.X + hotbarUI.grid.origin_position_offset.X
+    hotbarUI.grid.origin_position.Y = hotbarUI.grid.origin_position.Y + hotbarUI.grid.origin_position_offset.Y
 
-    love.graphics.draw(self.sprite, self.position.X, self.position.Y, nil, self.scale, self.scale, self.scale_offset.X, self.scale_offset.Y)
+    local main = love.graphics.draw(self.sprite, self.position.X, self.position.Y, nil, self.scale, self.scale, self.scale_offset.X, self.scale_offset.Y)
 
     for row, item in ipairs(hotbar) do
         if item.texture then
             row = row - 1
 
-            self.grid.origin_position.X = self.position.X - self.sprite:getWidth()/3.25
-            self.grid.origin_position.Y = self.position.Y
-
-            self.grid.origin_position.X = self.grid.origin_position.X + self.grid.origin_position_offset.X
-            self.grid.origin_position.Y = self.grid.origin_position.Y + self.grid.origin_position_offset.Y
-
-            local row_position = self.grid.origin_position.X + self.grid.padding * row
+            local item_row_position = self.grid.origin_position.X + self.grid.padding * row
 
             self.grid.item_scale_offset.X = item.texture:getWidth()/2
             self.grid.item_scale_offset.Y = item.texture:getHeight()/2
 
-            love.graphics.draw(
+            local item = love.graphics.draw(
                 item.texture,
-                row_position,
+                item_row_position,
                 self.grid.origin_position.Y,
-                nil, self.grid.item_scale, self.grid.item_scale,
+                nil,
+                self.grid.item_scale,
+                self.grid.item_scale,
                 self.grid.item_scale_offset.X,
                 self.grid.item_scale_offset.Y
             )
         end
     end
+    
+    local hotbar_selection_outline_index = hotbar.selection_outline_index - 1
+    local selection_outline_row_position = self.grid.origin_position.X + self.grid.padding * hotbar_selection_outline_index
+
+    local selection_outline = love.graphics.draw(
+        self.selection_outline_sprite,
+        selection_outline_row_position,
+        self.position.Y,
+        nil,
+        self.selection_outline_scale,
+        self.selection_outline_scale,
+        self.selection_outline_scale_offset.X,
+        self.selection_outline_scale_offset.Y
+    )
 end
 
 return hotbarUI
